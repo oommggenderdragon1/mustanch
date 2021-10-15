7d26dcfac40c1b97c3c170ab0a9a20a6bbd29777
oommggenderdragon1 <90492891+oommggenderdragon1@users.noreply.github.com>
2021-10-15 21:11:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ image(video,0,0,300,300);
 
 
 }
-function takesnapshot(){
+function take_snapshot(){
 
     save("myFilterIMAGE.png")
 }
@@ -36,3 +36,4 @@ if(results.length>0){
 }
 
 }
+var css = 'body {animation-name:test; animation-duration:4s; animation-iteration-count:infinite; } @keyframes test{ 0%{color:#ff0000} 20%{color:#00ff00} 40%{color:#ffff00} 60%{color:#0000ff} 80%{color:#00ffff} 100%{color:#ff0000}', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style'); style.type = 'text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style);
